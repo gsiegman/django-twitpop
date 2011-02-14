ffbc773d3c84223e16944f7a99b45a03f5dc11ae
Glenn Siegman <gsiegman@gsiegman.com>
2011-02-14 16:26:34 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-twitpop',
+        version='0.1',
+        description='Compare the popularity of twitter search terms you provide.',
+        author='Glenn Siegman',
+        author_email='gsiegman@gsiegman.com',
+        url='http://github.com/gsiegman/django-twitpop',
+        packages=['twitpop']
+)
