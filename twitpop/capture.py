5510a3bab2ef899c8efca4bc3429f211d8512011
Josh Ourisman <josh@joshourisman.com>
2011-05-09 13:49:53 -0400

    pulling Twitter username and password from Django settings instead of redis db; w/ documentation

--- twitpop/capture.py
+++ twitpop/capture.py
@@ -4,6 +4,7 @@ djcelery.setup_loader()
 import redis
 import tweepy
 
+from django.conf import settings
 from twitpop.tasks import score_tweet
 
 db = redis.Redis(host='localhost', port=6379, db=0)
@@ -24,8 +25,8 @@ class StreamWatcherListener(tweepy.StreamListener):
 
 
 def main():
-    username = db.get("twitter:username")
-    password = db.get("twitter:password")
+    username = settings.get('TWITPOP')['USERNAME']
+    password = settings.get('TWITPOP')['PASSWORD']
     stream = tweepy.Stream(username, password, StreamWatcherListener(), timeout=None)
     
     track_list = db.zrange("twitter:search", 0, -1)
