105f339da4fbdd390d94e8a13f6124a910ebbbc2
Glenn Siegman <gsiegman@gsiegman.com>
2011-10-17 16:29:24 -0400

    bumped to version 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='django-twitpop',
-        version='0.2',
+        version='0.3',
         description='Compare the popularity of twitter search terms you provide.',
         author='Glenn Siegman',
         author_email='gsiegman@gsiegman.com',
