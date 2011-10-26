a3805cdb109b014988ecdfb48bdc2bd8f9cc8f7e
Glenn Siegman <gsiegman@gsiegman.com>
2011-10-26 11:23:36 -0400

    using tweetstream instead of tweepy which stopped working

--- twitpop/capture.py
+++ twitpop/capture.py
@@ -2,34 +2,26 @@ import djcelery
 djcelery.setup_loader()
 
 import redis
-import tweepy
+import tweetstream
 
 from twitpop.tasks import score_tweet
 
 db = redis.Redis(host='localhost', port=6379, db=0)
 
-class StreamWatcherListener(tweepy.StreamListener):
-    
-    def on_status(self, status):
-        try:
-            score_tweet.delay(status.text)
-        except Exception as exception:
-            pass
-    
-    def on_error(self, status_code):
-        return True  # keep stream alive
-    
-    def on_timeout(self):
-        pass
-
 
 def main():
     username = db.get("twitter:username")
     password = db.get("twitter:password")
-    stream = tweepy.Stream(username, password, StreamWatcherListener(), timeout=None)
-    
     track_list = db.zrange("twitter:search", 0, -1)
-    stream.filter([], track_list)
+    
+    with tweetstream.FilterStream(username, password, track=track_list) as stream:
+        for tweet in stream:
+            text = tweet["text"]
+
+            try:
+                score_tweet.delay(text)
+            except Exception as exception:
+                pass
 
 
 if __name__ == '__main__':
